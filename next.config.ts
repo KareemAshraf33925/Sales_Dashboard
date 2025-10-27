b989c7f4f43dcc71349c3b12e8f988270a689204
KareemAshraf33925 <ashrafkareem005@gmail.com>
2025-10-27 12:48:28 -0700

    configexport

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
-    // output:"export",
-    // assetPrefix:"/Sales_Dashboard/",
-    // basePath:"/Sales_Dashboard",
-    // images:{
-    //     domains:['KareemAshraf33925.github.io']
-    // }
+    output:"export",
+    assetPrefix:"/Sales_Dashboard/",
+    basePath:"/Sales_Dashboard",
+    images:{
+        domains:['KareemAshraf33925.github.io']
+    }
 };
  
 const withNextIntl = createNextIntlPlugin();
